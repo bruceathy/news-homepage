a4300b2990823f733bd9478a7d3b384652248a8b
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-09-13 18:13:47 -0400

    navbar attempt

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const navLinks = document.getElementById("nav-links");
+const toggleBtn = document.getElementById("toggle-btn");
 
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.add("show");
+  //   alert("hello");
+});
