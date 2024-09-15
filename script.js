04636691849af6429c8e5697692571ea6cede7ee
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-09-14 23:40:24 -0400

    mobile navbar finish

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const navLinks = document.getElementById("nav-links");
 const toggleBtn = document.getElementById("toggle-btn");
+const closeBtn = document.getElementById("close-btn");
 
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.add("show");
   //   alert("hello");
+});
+
+closeBtn.addEventListener("click", () => {
+  navLinks.classList.remove("show");
 });
